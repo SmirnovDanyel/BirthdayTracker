3b00b492a9ce63837ad49258d995f9b94feabad1
SmirnovDanyel <smirnovdanyel@gmail.com>
2023-05-10 13:38:40 +0300

    Initial Commit

---
+++ BirthdayTracker/AddBirthdayViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  BirthdayTracker
+//
+//  Created by Даниил Смирнов on 05.05.2023.
+//
+
+import UIKit
+import CoreData
+import UserNotifications
+
+class AddBirthdayViewController: UIViewController {
+    
+        @IBOutlet var firstNameTextField: UITextField!
+        @IBOutlet var lastNameTextField: UITextField!
+        @IBOutlet var birthdatePicker: UIDatePicker!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        birthdatePicker.maximumDate = Date()
+    }
+    
+    @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+        print("Нажата кнопка сохранения.")
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        let birthdate = birthdatePicker.date
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let newBirthday = Birthday(context: context)
+        newBirthday.firstName = firstName
+        newBirthday.lastName = lastName
+        newBirthday.birthdate = birthdate as Date?
+        newBirthday.birthdayId = UUID().uuidString
+        
+        if let uniqueId = newBirthday.birthdayId {
+            print("birthdayId: \(uniqueId)")
+        }
+        do {
+            try context.save()
+            let message = "Сегодня \(firstName) \(lastName) прзднует день рождения!"
+            let content = UNMutableNotificationContent()
+            content.body = message
+            content.sound = UNNotificationSound.default
+            var dateComponents = Calendar.current.dateComponents([.month, .day], from: birthdate)
+            dateComponents.hour = 8
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+            if let identifier = newBirthday.birthdayId {
+                let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+                let center = UNUserNotificationCenter.current()
+                center.add(request, withCompletionHandler: nil)
+            }
+        } catch let error {
+            print("Не удалось сохранить из-за ошибки \(error) .")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
